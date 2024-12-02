f2dd3edb8bdb396cc44483d2a2a89d29227ca072
nemanja <antonijevicnemanja68@gmail.com>
2024-12-02 20:11:04 +0100

    update

--- src/constants/imgUrl.ts
+++ src/constants/imgUrl.ts
@@ -1,1 +1,1 @@
-export const imgUrl = 'https://media.licdn.com/dms/image/v2/D4D03AQEgCw_yR2zwPg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1732814610958?e=1738195200&v=beta&t=IfUCd5zjqgvQDtzpf8yrBg6YJKZ4cveQD4nPXFsQJho'
+export const imgUrl = 'https://media.licdn.com/dms/image/v2/D4D03AQHixiqFOxpesg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1733007654924?e=1738800000&v=beta&t=UgByNpg0HsPg1Fnb5D6-XkSLS3gy5PfX1PkKWb68jes'
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -9,10 +9,15 @@ const Projects = () => {
   useAnimation(ref, 1)
 
   return (
-    <div ref={ref} className="mt-12">
-      <h1 className="text-4xl text-primary font-bold text-center">Projects</h1>
-      <div className="flex justify-center mt-5">
+    <div ref={ref} className="flex justify-center mt-12">
+      <div className="md:flex hidden justify-center w-1/4 ml-32 mt-20">
+        <p className="text-xl font-bold tracking-wide">
+        All my projects <b className="text-primary">showcase my experience and knowledge in frontend development</b>, check out my other projects on my <a className="underline text-primary" href="https://github.com/Nemanja0033">GitHub</a>.
+        </p>
+      </div>
+      <div className="flex justify-center mt-5 md:mr-52 mr-0">
         <div className="flex-row">
+         <h1 className="text-4xl text-primary font-bold text-center">Projects</h1>
           <div className="mb-8">
             <ProjectsCard
             title="Threadsy Social Network"
