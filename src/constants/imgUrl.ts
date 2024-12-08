05cd1424bc178a542f906108710962c86083476b
nemanja <antonijevicnemanja68@gmail.com>
2024-12-08 18:17:09 +0100

    Link change

--- src/constants/imgUrl.ts
+++ src/constants/imgUrl.ts
@@ -1,1 +1,1 @@
-export const imgUrl = 'https://media.licdn.com/dms/image/v2/D4D03AQHixiqFOxpesg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1733007654924?e=1738800000&v=beta&t=UgByNpg0HsPg1Fnb5D6-XkSLS3gy5PfX1PkKWb68jes'
+export const imgUrl = 'https://media.licdn.com/dms/image/v2/D4D03AQHbfdhdTxlOjw/profile-displayphoto-shrink_200_200/B4DZOOp_.CHcAY-/0/1733265195633?e=1739404800&v=beta&t=v3TvQ9D_d3ixRuBBNxeMR4TNhzec_t2_7nFFvCTRu7U'
