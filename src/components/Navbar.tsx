086c5403df3137aa18e0e066c17140cf86061f49
nemanja <antonijevicnemanja68@gmail.com>
2025-02-12 15:26:11 +0100

    FEAT:: new hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,42 @@
 import { Link } from "react-router-dom"
 import ThemeToggler from "./ThemeToggler"
-import { Palette } from "lucide-react"
+import { Menu, Palette, X } from "lucide-react"
 import { urlBlog } from "../constants/blogUrl"
+import { useState } from "react"
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    if(isOpen){
+        return(
+            <div className="w-full h-screen flex justify-center items-center">
+                <div className="grid grid-cols-1 gap-10 text-2xl">
+                <button onClick={() => setIsOpen(false)}><X /></button>
+                <Link className="hover:text-primary" to={'/'}>Home</Link>
+                <Link  className="hover:text-primary" to={'/projects'}>Projects</Link>
+                <Link  className="hover:text-primary" to={'/about'}>About</Link>
+                <Link className="hover:text-primary" to={urlBlog}>Blog</Link>
+                <ThemeToggler />
+                </div>
+            </div>
+        )
+    }
+
   return (
-    <nav className="w-full h-[60px] flex justify-evenly items-center shadow-md">
+    <>
+    <nav className="md:hidden flex justify-between px-5 h-[60px] w-full items-center shadow-md">
+        <Link to={'/'}>
+                <div className="flex justify-center gap-2 cursor-pointer items-center">
+                    <span className="flex gap-2 text-xl font-semibold">Portfolio<Palette className="text-primary" /></span>
+                </div>
+        </Link>
+
+        <button onClick={() => setIsOpen(!isOpen)}>
+            <Menu />
+        </button>
+    </nav>
+
+    <nav className="w-full h-[60px] md:flex hidden justify-evenly items-center shadow-md">
         <Link to={'/'}>
             <div className="flex justify-center gap-2 cursor-pointer items-center">
                 <span className="flex gap-2 text-xl font-semibold">Portfolio<Palette className="text-primary" /></span>
@@ -21,6 +52,7 @@ const Navbar = () => {
             </div>
         </div>
     </nav>
+    </>
   )
 }
 
